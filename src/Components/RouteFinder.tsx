72ad0ef61d2a9a9fdecd150844cfe353b7f68d4e
j4zuu <jasu.honkanen2@gmail.com>
2021-11-03 14:10:50 +0200

    pathfinding works

--- src/Components/RouteFinder.tsx
+++ src/Components/RouteFinder.tsx
@@ -4,18 +4,16 @@ import {SvgIcon} from "@material-ui/core";
 
 
 function RouteFinder() {
-    /*
     // Get vector length by it's id
-    let lengthGetter = (id: String) => {
-        let tempLines: NodeListOf<SVGGeometryElement> = document.querySelectorAll(`*[id*=_${id}_]`);
-        let tempLine: SVGGeometryElement = tempLines[0] // SVGGeometryElement
-        return tempLine.getTotalLength();
+    let lengthGetter = (id: any) => {
+        const divElement: SVGGeometryElement = lines[id].current;
+        return divElement.getTotalLength()
     };
 
     let graph: any = {
         START: {A: 1},
         A: {START: 1, B: 1},
-        B: {AA: 1, C: 1, E: 1},
+        B: {A: 1, C: 1, E: 1},
         C: {B: 1, D: 1},
         D: {C: 1, G: 1},
         E: {B: 1, FINISH: 1},
@@ -23,20 +21,44 @@ function RouteFinder() {
         FINISH: {G: 1, E: 1},
     };
 
+    const STARTA: any = useRef();
+    const AB: any = useRef();
+    const BC: any = useRef();
+    const CD: any = useRef();
+    const BE: any = useRef();
+    const EFINISH: any = useRef();
+    const DG: any = useRef();
+    const GFINISH: any = useRef();
+
+    const lines: any = {
+        STARTA: STARTA,
+        AB: AB,
+        BC: BC,
+        CD: CD,
+        BE: BE,
+        EFINISH: EFINISH,
+        DG: DG,
+        GFINISH: GFINISH
+    }
+
 // Iterate trough object and change the vector lengths to match the lines
     let objIterator = (obj: Object) => {
         for (const [key, value] of Object.entries(obj)) {
             let tempObj: any = {};
             for (let [key1, value1] of Object.entries(value)) {
                 const temp = key + key1;
-                value1 = lengthGetter(temp);
+                const temp2 = key1 + key
+                if (Object.keys(lines).includes(temp)) {
+                    value1 = lengthGetter(temp);
+                } else value1 = lengthGetter(temp2)
+                //Set length property of lines
                 tempObj[key1] = value1;
             }
             graph[key] = tempObj;
         }
+        console.log(graph)
     };
 
-    objIterator(graph);
 
     let shortestDistanceNode = (distances: any, visited: Array<any>) => {
         // create a default value for shortest
@@ -59,11 +81,6 @@ function RouteFinder() {
     };
 
     let findShortestPath = (graph: any, startNode: string, endNode: string) => {
-        // Set all lines with "_" to hidden
-        let tempLine2: NodeListOf<SVGGeometryElement> = document.querySelectorAll(`*[id*="_"]`);
-        for (let j = 0; j < tempLine2.length; j++) {
-            tempLine2[j].style.visibility = 'hidden';
-        }
         // track distances from the start node using a hash object
         let distances: any = {};
         distances[endNode] = 'Infinity';
@@ -122,14 +139,20 @@ function RouteFinder() {
         for (let i = 0; i < shortestPath.length - 1; i++) {
             let tempString: string = shortestPath[i]
             let tempString1: string = shortestPath[i + 1]
+            let finalName: string
             let tempName: any = tempString.concat(tempString1)
-            tempName.toString();
+            let tempName1: any = tempString1.concat(tempString)
+            if (Object.keys(lines).includes(tempName)) {
+                finalName = tempName;
+            } else finalName = tempName1
             /**
-             * TODO: get correct selector for SVG lines
+             * TODO: get correct selector for SVG lines and make them visible
              **/
-    /*
-            let tempLine1: NodeListOf<SVGGeometryElement> = document.querySelectorAll(`*[id*=_${tempName}_]`);
-            tempLine1[0].style.visibility = 'visible';
+            console.log("Tempname: " + finalName)
+            const divElement: SVGGeometryElement = lines[finalName].current;
+            divElement.style.visibility = "visible"
+
+
         }
         //this is the shortest path
         let results = {
@@ -140,34 +163,27 @@ function RouteFinder() {
         // return the shortest path & the end node's distance from the start node
     };
 
-    const activeSlideRef = useRef(null);
-*/
-
-    const test = () => {
-
-    }
-
 
     /**
      * check line names ea. AB and if it's not in testArr (that includes all lines), reverse them (=BA) and check again.
-     * Add the matching line to pathArr and in the end loop trough all of them and set visibility to "visible" while all others are "none"
+     * Add the matching line to lines and in the end loop trough all of them and set visibility to "visible" while all others are "none"
      */
 
-    const elementRef: any = useRef();
-    const elementRef2: any = useRef();
-    const elementRef3: any = useRef();
-    const elementRef4: any = useRef();
-    const AA: any = useRef()
 
-    const testArr = []
-    testArr.push(AA)
 
-    const pathArr = []
-    pathArr.push(testArr[])
+    const pathLooper = () => {
+        for (let i = 0; i < lines.length; i++) {
+            const divElement: SVGGeometryElement = lines[i].current;
+            console.log(divElement.getTotalLength())
+            divElement.style.visibility = "visible"
+        }
+    }
+
 
     useEffect(() => {
-        const divElement = elementRef.current;
-        divElement.style.display = "none"
+        pathLooper()
+        objIterator(graph);
+        findShortestPath(graph, "FINISH", "START")
     }, []);
 
 
@@ -183,13 +199,6 @@ function RouteFinder() {
 
     return (
         <>
-            <div ref={elementRef}>
-                I'm an element
-            </div>
-            <div ref={elementRef2}>
-                I'm an element2
-            </div>
-
             <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="500"
@@ -197,50 +206,57 @@ function RouteFinder() {
                 viewBox="0 0 500 500"
             >
                 <path
-                    ref={AA}
                     fill="#FDFDFD"
                     d="M68.087 50.125H366.33299999999997V406.432H68.087z"
-                ></path>
+                />
                 <path
+                    ref={STARTA}
                     stroke="#000"
                     d="M309.942 112.039L306.6 169.266"
                     visibility="hidden"
-                ></path>
+                />
                 <path
+                    ref={AB}
                     stroke="#000"
                     d="M306.182 170.101L233.918 196.417"
                     visibility="hidden"
-                ></path>
+                />
                 <path
+                    ref={BC}
                     stroke="#000"
                     d="M235.589 198.088L342.941 271.605"
                     visibility="hidden"
-                ></path>
+                />
                 <path
+                    ref={CD}
                     stroke="#000"
                     d="M342.941 272.44L236.424 361.83"
                     visibility="hidden"
-                ></path>
+                />
                 <path
+                    ref={BE}
                     stroke="#000"
                     d="M234.336 196.927L124.478 211.547"
                     visibility="hidden"
-                ></path>
+                />
                 <path
+                    ref={EFINISH}
                     stroke="#000"
                     d="M124.06 211.965L185.046 327.671"
                     visibility="hidden"
-                ></path>
+                />
                 <path
+                    ref={DG}
                     stroke="#000"
                     d="M235.171 362.341L232.247 226.167"
                     visibility="hidden"
-                ></path>
+                />
                 <path
+                    ref={GFINISH}
                     stroke="#000"
                     d="M232.247 226.167L186.299 328.089"
                     visibility="hidden"
-                ></path>
+                />
                 <text
                     style={{whiteSpace: "pre"}}
                     x="309.524"
